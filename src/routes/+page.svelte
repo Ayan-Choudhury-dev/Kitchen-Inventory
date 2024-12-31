adecc3038e5ba87a8f48ed1a1f505c835c58c6eb
Ayan-Choudhury-dev <ayan26199+github@gmail.com>
2024-12-31 18:03:06 +0530

    Add Input component and implement addition functionality in the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,46 +2,54 @@
   import { invoke } from "@tauri-apps/api/core";
   import "../app.css";
   import Button from "@/components/ui/button/button.svelte";
+  import Input from "@/components/ui/input/input.svelte";
 
   let name = "";
   let greetMsg = "";
+  let num1 = 0;
+  let num2 = 0;
+  let sum = 0;
 
 
-  async function greet(event: Event) {
+
+  // async function greet(event: Event) {
+  //   event.preventDefault();
+  //   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  //   if(name === "") {
+  //     greetMsg = "Please enter a name!";
+  //     return;
+  //   }
+  //   greetMsg = await invoke("greet", { name });
+  // }
+
+  async function add(event: Event) {
     event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    if(name === "") {
-      greetMsg = "Please enter a name!";
-      return;
-    }
-    greetMsg = await invoke("greet", { name });
+    sum = await invoke("add", { a: parseInt(num1), b: parseInt(num2) });
   }
 </script>
 
 <main class="container">
   <h1>Welcome to Tauri + Svelte</h1>
- <div class=" text-blue-600 py-4">Lorem ipsum</div>
 
 
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://kit.svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
-    </a>
-  </div>
-  <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
 
-  <form class="row" onsubmit={greet}>
+ <form class="row flex " onsubmit={add}>
+    <Input id="num1" placeholder="Enter a number..." bind:value={num1} />
+    <Input id="num2" placeholder="Enter a number..." bind:value={num2} />
+     <!-- <input id="num1" type="number" placeholder="Enter a number..." bind:value={num1} />
+    <input id="num2" type="number" placeholder="Enter a number..." bind:value={num2} /> -->
+    <Button type="submit">Add</Button>
+ </form>
+
+  <p>Sum: {sum}</p>
+
+<!--  -->
+
+
+  <!-- <form class="row" onsubmit={greet}>
     <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
     <Button type="submit">Greet</Button>
   </form>
-
-
-  <p>{greetMsg}</p>
+  <p>{greetMsg}</p> -->
 </main>
 
